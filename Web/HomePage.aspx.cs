85d0b50719f111ed909f0555d66440bb8414f0fc
laksiri bandara <lakb2011@hotmail.com>
2016-12-20 23:58:43 +0530

    d

---
+++ Web/HomePage.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cheque_Ordering_system.Web
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //--------Used SP
+            //-----> spGetEmpCurrentTimeDetails, spGetEmpCurrentTimeDetailsBrDept, spGetEmpBrancheDeptAttenDetails
+
+            //--------2016/04/25 Actual Timein maybe less than Recored Timein (requested by Nuwan Weragala) ---> Isuru
+            //--------2016/04/25 The date Actual-Time-In Date can enter follwoing Date ---> Isuru
+            string a = string.Empty();
+        }
+    }
+}
